3c5d9e63a21b2248498bddbed70c75a5525460e3
Jin Zhu <zhujin1121@gmail.com>
2022-08-03 09:57:28 +0800

    fix seg_evaluation bug, support labels and samples

--- metrics/seg_evaluation.py
+++ metrics/seg_evaluation.py
@@ -32,22 +32,26 @@ class SegmentationEvaluation(BasicEvaluation):
                 'dice_{}'.format(_) for _ in classes
             ]
 
-    def __call__(self, pred_label, sample):
+    def __call__(self, pred_labels, samples):
         """
 
-        :param pred_label: H x W x 1, tensor int
-        :param sample: {'gt': tensor int H x W x 1}
-        :return: {'id': , 'imgs': [pred_label, gt_label], 'dice_x': ,}
+        :param pred_labels: list of H x W x 1, tensor int
+        :param samples: list of {'gt': tensor int H x W x 1}
+        :return: {'id': 'dice_x': ,}
         """
-        gt_label = sample['gt']
-        report = {'imgs': [pred_label, gt_label]}
-        for l, m in enumerate(self.metrics, 1):
-            gt = gt_label == l
-            pred = pred_label == l
-            dice = self.dice_coef(gt, pred)
-            report[m] = dice
-        report['id'] = sample['id']
-        return report
+        reports = {}
+        for m in self.metrics:
+            reports[m] = []
+        for pred_label, sample in zip(pred_labels, samples):
+            gt_label = sample['gt']
+            for l, m in enumerate(self.metrics, 1):
+                gt = gt_label == l
+                pred = pred_label == l
+                dice = self.dice_coef(gt, pred)
+                reports[m].append(dice)
+        for m in reports:
+            reports[m] = np.mean(reports[m])
+        return reports
 
     @staticmethod
     def dice_coef(gt, pred, eps=1e-6):
