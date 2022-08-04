a7417a114c8c37e2347eb4d6103c729bafafc368
Jin Zhu <zhujin1121@gmail.com>
2022-08-04 15:18:21 +0800

    transSR with UNet evaluation

--- test.py
+++ test.py
@@ -13,6 +13,8 @@ parser.add_argument('--config-file', type=str, required=True, metavar='CONFIG',
                     help='Path to config file.')
 parser.add_argument('--gpu-id', type=int, metavar='GPU',
                     help='Which gpu to use.')
+parser.add_argument('--folder', type=str, metavar='DATA-FOLDER',
+                    help='data folder')
 
 args = parser.parse_args()
 # do distributed training here
@@ -20,6 +22,12 @@ config_file = args.config_file
 gpu_id = args.gpu_id
 
 paras = ParametersLoader(config_file)
+
+# change data_folder
+data_folder = args.folder
+if data_folder is not None:
+    paras.data_folder = '/'.join(paras.data_folder.split('/')[:-1] + [data_folder])
+    paras.model_name = 'OASIS_Segmentation_{}'.format(data_folder.replace('FR_SR_OASIS_', ''))
 
 if gpu_id is not None:
     paras.gpu_id = gpu_id
